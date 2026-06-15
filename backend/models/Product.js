06b6f14e1ce7a5b38fc5a07ea90f5b7d44508fcb
techprav7 <tech.prav7@gmail.com>
2026-06-15 10:33:06 +0530

    field name changed and api keys config

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -57,7 +57,7 @@ const productSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
-    isNew: {
+    isNewArrival: {
       type: Boolean,
       default: false,
     },
